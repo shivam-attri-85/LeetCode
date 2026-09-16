87e7f3e00dd3c6b61810a24479a8df1f9655118c
Shivam chaudhary <shivamkumarattri85@gmail.com>
2026-09-16 22:11:17 +0530

    Time: 0 ms (100%), Space: 11.8 MB (90.74%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-// bool leaf(TreeNode*root){
-//     if(root->left==nullptr &&root->right==nullptr){
-//         return true;
-//     }
-//     return false;
-// }
+bool leaf(TreeNode*root){
+    if(root->left==nullptr &&root->right==nullptr){
+        return true;
+    }
+    return false;
+}
 
 int solve(TreeNode*root,int curr){
     if(root==nullptr){return 0;
     }
     curr=(curr*10)+root->val;
-    if(root->left==nullptr && root->right==nullptr){
+    if(leaf(root)){
         return curr;
     }
     int l=solve(root->left,curr);
